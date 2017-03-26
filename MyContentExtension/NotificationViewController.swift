5c9129737a9ec65ccb5e9dd7ee11fe822802c9d0
Shin Park <shinpark@shins-mac-mini.home>
2017-03-25 22:42:06 -0500

    NewNotifications - custom notification extension and actions

---
+++ MyContentExtension/NotificationViewController.swift
@@ -0,0 +1,61 @@
+//
+//  NotificationViewController.swift
+//  MyContentExtension
+//
+//  Created by Shin Park on 3/25/17.
+//  Copyright © 2017 shinDev. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        
+        guard let attachment = notification.request.content.attachments.first else {
+            return
+        }
+        
+        if attachment.url.startAccessingSecurityScopedResource() {
+            let imageData = try? Data.init(contentsOf: attachment.url)
+            
+            if let img = imageData {
+                imageView.image = UIImage(data: img)
+            }
+        }
+    }
+    
+    func didReceive(_ response: UNNotificationResponse, completionHandler completion: @escaping (UNNotificationContentExtensionResponseOption) -> Void) {
+        
+        if response.actionIdentifier == "fistBump" {
+            completion(.dismissAndForwardAction)
+        } else if response.actionIdentifier == "dismiss" {
+            completion(.dismissAndForwardAction)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- NewNotifications/NotifyVC.swift
+++ NewNotifications/NotifyVC.swift
@@ -7,19 +7,89 @@
 //
 
 import UIKit
+import UserNotifications
 
 class NotifyVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 1. REQUEST PERMISSION
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            
+            if granted {
+                print("Notification access granted")
+            } else {
+                print(error?.localizedDescription as Any)
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func notifyButtonTapped(sender: UIButton) {
+        scheduleNotification(inSeconds: 5, completion: { success in
+            if success {
+                print("Successfully scheduled notification")
+            } else {
+                print("Error scheduling notification")
+            }
+        })
     }
-
-
+    
+    func scheduleNotification(inSeconds: TimeInterval, completion: @escaping (_ Success: Bool) -> ()) {
+        
+        // Add an attachment
+        let myImage = "rick_grimes"
+        guard let imageUrl = Bundle.main.url(forResource: myImage, withExtension: "gif") else {
+            completion(false)
+            return
+        }
+        
+        var attachment: UNNotificationAttachment
+        
+        attachment = try! UNNotificationAttachment(identifier: "myNotification", url: imageUrl, options: .none)
+        
+        
+        let notif = UNMutableNotificationContent()
+        
+        // ONLY FOR EXTENSION
+        notif.categoryIdentifier = "myNotificationCategory"
+        
+        notif.title = "New Notification"
+        notif.subtitle = "These are great"
+        notif.body = "The new notification options in iOS 10 are what I've always dreamed of!"
+        
+        notif.attachments = [attachment]
+        
+        let notifTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "myNotification", content: notif, trigger: notifTrigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            if error != nil {
+                print(error as Any)
+                completion(false)
+            } else {
+                completion(true)
+            }
+        })
+    }    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
